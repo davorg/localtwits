cc6bd8a563fe00b6b6e22601d02181f79d862737
Dave Cross <dave@dave.org.uk>
2010-09-29 20:41:50 +0100

    Switch to OAuth. Use cursors to fetch followers and friends. Don't try to follow protected people.

--- build.pl
+++ build.pl
@@ -10,11 +10,26 @@ use YAML 'LoadFile';
 my $config = LoadFile(shift || 'config.yaml');
 
 my $t = Net::Twitter->new({
-  username => $config->{username},
-  password => $config->{password},
+  traits => [qw(API::REST OAuth)],
+  consumer_key => $config->{consumer_key},
+  consumer_secret => $config->{consumer_secret},
 }) or die;
 
+my ($access_token, $access_token_secret) = retrieve_tokens();
+
+if ($access_token && $access_token_secret) {
+  $t->access_token($access_token);
+  $t->access_token_secret($access_token_secret);
+}
+
+unless ($t->authorized()) {
+  get_twitter_authorization();
+  exit();
+}
+
 my $follows = autofollow($t);
+
+exit unless $follows;
 
 my $tweets = $t->friends_timeline({count => $config->{tweets} || 20});
 
@@ -29,22 +44,28 @@ sub autofollow {
   my $t = shift;
 
   my %follow;
-  my $follow = $t->friends;
 
-  unless ($follow) {
-    warn "$t->http_message\n";
-    return;
+  for (my $cur = -1, my $r; $cur; $cur = $r->{next_cursor}) {
+    $r = $t->friends({cursor => $cur});
+
+    foreach (@{$r->{users}}) {
+      $follow{$_->{screen_name}} = 1;
+    }
   }
 
-  foreach (@$follow) {
-    $follow{$_->{screen_name}} = 1;
-  }
+#  unless ($follow) {
+#    warn scalar localtime() . "\n";
+#    warn $t->http_message . "\n";
+#    warn $t->{_twitter_error}{error} . "\n";
+#    return;
+#  }
 
   my %nofollow;
   if (-e 'nofollow') {
     open my $nf, '<', 'nofollow' or die $!;
     while (<$nf>) {
       chomp;
+      s/\s+//;
       $nofollow{$_} = 1;
 
       if ($follow{$_}) {
@@ -54,13 +75,74 @@ sub autofollow {
     }
   }
 
-  if (defined(my $f = $t->followers)) {
-    foreach (@{$f}) {
+  my %followers;
+
+  for (my $cur = -1, my $r; $cur; $cur = $r->{next_cursor}) {
+    $r = $t->followers({cursor => $cur});
+
+    foreach (@{$r->{users}}) {
       unless ($nofollow{$_->{screen_name}} or $follow{$_->{screen_name}}) {
+        if ($_->{protected}) {
+          warn "Protected user $_->{screen_name} following\n";
+          warn "Not following back\n";
+          next;
+        }
         $t->create_friend($_->{screen_name});
+        $follow{$_->{screen_name}} = 1;
       }
     }
   }
 
   return \%follow;
+}
+
+sub get_twitter_authorization {
+  print "Visit the following URL in your browser to authorize this app:\n" .
+    $t->get_authorization_url() . "\n";
+  print "Once done, enter the PIN# ";
+    
+  my $pin = <STDIN>; # wait for user input
+  chomp $pin;
+
+  eval {
+    print "PIN received, contacting twitter to obtain access tokens...\n";
+    my ($access_token, $access_token_secret, $user_id, $screen_name) =
+      $t->request_access_token(verifier => $pin);
+
+    if ($access_token && $access_token_secret) {
+      print "Tokens received, storing to .tokens...\n";
+      store_tokens($access_token, $access_token_secret);
+      print "Done. Please run program again.\n";
+    } else {
+      print "Twitter error: did not receive access tokens.\n";
+    }
+  };
+
+  if (my $err = $@) {
+    die "Twitter error: ", $err->error(), "\n";
+  }
+}
+
+sub retrieve_tokens {
+  if (-e '.tokens') {
+    open my $tok, '<', '.tokens'
+      or die ".tokens: $!\n";
+
+    chomp(my $token = <$tok>);
+    chomp(my $secret = <$tok>);
+
+    return ($token, $secret);
+  }
+
+  return;
+}
+
+sub store_tokens {
+  my ($a_token, $a_token_secret) = @_;
+
+  open my $tok, '>', '.tokens' or
+    die ".tokens: $!\n";
+
+  print $tok $a_token, "\n";
+  print $tok $a_token_secret, "\n";
 }
